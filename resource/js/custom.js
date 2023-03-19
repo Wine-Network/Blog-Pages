003b8b860499bc9dc43b717abbe8cb7468bea578
xiaowine <68009112+xiaowine@users.noreply.github.com>
2023-03-19 02:43:17 +0000

    Deploying to gh-pages from @ xiaowine/hexo-theme-butterfly@7260b7253338ab6bb6be62e37889956e807518cd 🚀

---
+++ resource/js/custom.js
@@ -0,0 +1,13 @@
+// let textContent = ""
+// let siteName = document.querySelector('.site-name')
+// siteName.onmouseover = function () {
+//     if (textContent === "") {
+//         textContent = siteName.textContent
+//     }
+//     siteName.textContent = " 🏠 "
+//
+// };
+//
+// siteName.onmouseout = function () {
+//     siteName.textContent = textContent
+// };
